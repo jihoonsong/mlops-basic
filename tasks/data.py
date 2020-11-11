afae198e9ed1c1ca30327c1b66f25d5f848b037b
Jihoon Song <jihoonsong@Jihoons-MacBook-Pro.local>
2020-11-11 21:13:07 +0900

    Use argparse module to receive an argument
    Receive GCP project id from the command line.

--- tasks/data.py
+++ tasks/data.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import tensorflow as tf
 
@@ -44,4 +45,22 @@ def save_data_as_csv(x_train, y_train, x_test, y_test):
 
 
 if __name__ == "__main__":
+    description = """
+    This script generates fashion_mnist data and upload it as a
+    BigQuery dataset under [PROJECT ID].fashion_mnist.{train,test}.
+
+    To run this script successfully, you need to complete the following steps:
+    1. Select or create a Cloud Platform project.
+    2. Enable billing for your project.
+    3. Enable the Google Cloud BigQuery API.
+    4. Setup Authentication.
+
+    Please see (https://googleapis.dev/python/bigquery/latest/index.html) for more information."""
+
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--project', type=str, default='mlops-basic', help='Your GCP project id')
+
+    args = parser.parse_args()
+
     save_data_as_csv(*generate_data())
